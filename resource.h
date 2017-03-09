0b2d946be94daf6049d5f01bb90662d962bcfea5
Mihail Konev <k.mvc@ya.ru>
2017-03-09 22:09:32 +0500

    remove all files
    Prepares for the following "lw" program history.

--- resource.h
+++
@@ -1,8 +0,0 @@
-#define IDI_ICON_TRAY        101
-#define IDI_ICON_TRAY_PAUSE  102
-#define IDI_ICON_TRAY_PASS   103
-#define IDI_ICON_TRAY_UTF16  104
-
-#define IDS_SUSPEND 121
-#define IDS_QUIT    122
-#define IDS_PASS    123
